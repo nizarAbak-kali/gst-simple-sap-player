c0fcc9a78fda07c3145401589e36b821da751d5d
ABAK KALI Nizar <nizar.abakkali@external.thalesgroup.com>
2018-11-23 12:09:36 +0100

    firt commit -> TODO : README

---
+++ player.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import os
+import time
+
+import gi
+gi.require_version('Gst', '1.0')
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gst, GObject, Gtk
+from  rcv_mc_from import SapReceiver, SdpModel
+
+class GTK_Main(object):
+
+    def __init__(self):
+        #SAP PART
+        self.session_name_changed = False
+        self.sap_rcv = SapReceiver()
+
+        self.sap_rcv.start()
+        # IHM PART
+        window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
+        window.set_title("Simple test Player")
+        window.set_default_size(200, 200)
+        window.connect("destroy", Gtk.main_quit, "WM destroy")
+        vbox = Gtk.VBox()
+        window.add(vbox)
+        hbox = Gtk.HBox()
+        vbox.pack_start(hbox, False, False, 0)
+
+        self.entry = Gtk.ListStore(str)
+
+        while len(self.sap_rcv.sdp_dict.keys()) == 0:
+            print ("waiting ....")
+            time.sleep(1)
+            pass
+
+        time.sleep(1)
+
+        name_list = list(self.sap_rcv.sdp_dict.keys())
+
+        print(name_list)
+        self.current_session_name = name_list [0]
+
+        name_combo = Gtk.ComboBoxText()
+        name_combo.set_entry_text_column(0)
+        name_combo.connect("changed", self.on_name_combo_changed)
+        for i in name_list:
+            name_combo.append_text(i)
+        hbox.pack_start(name_combo, False, False, 0)
+
+        self.button = Gtk.Button("Start")
+        vbox.pack_start(self.button, False, False, 0)
+        self.button.connect("clicked", self.start)
+
+        self.button = Gtk.Button("Stop")
+        hbox.pack_start(self.button, False, False, 0)
+        self.button.connect("clicked", self.stop)
+
+        self.label = Gtk.Label('lol')
+        self.label.set_line_wrap(True)
+        vbox.pack_start(self.label, False, False, 0)
+
+
+        self.movie_window = Gtk.DrawingArea()
+        vbox.add(self.movie_window)
+        window.show_all()
+
+        # GSTREAMER PART
+        self.start_pipeline()
+        bus = self.pipeline.get_bus()
+        bus.add_signal_watch()
+        bus.enable_sync_message_emission()
+        bus.connect("message", self.on_message)
+
+
+    def on_name_combo_changed(self, combo):
+        text = combo.get_active_text()
+        print("selected : ", text)
+        self.current_session_name = text
+        self.session_name_changed = True
+
+    def start_pipeline(self):
+        sdp = self.sap_rcv.sdp_dict[self.current_session_name]
+        self.label.set_text(str(sdp))
+        gst_command='udpsrc'+\
+                    ' address='+sdp.ip+ \
+                    ' port='+str(sdp.port)+\
+                    ' multicast-iface=eth0'+ \
+                    ' caps="'+ sdp.get_caps_from_sdp()+'" '
+
+
+
+        if sdp.encoding_type  == "RAW":
+            gst_command += " ! rtpvrawdepay ! glimagesink sync = false"
+        else :
+            gst_command += " ! rtph264depay ! h264parse ! avdec_h264 ! glimagesink sync = false"
+        self.pipeline = Gst.parse_launch(gst_command)
+        self.pipeline.set_state(Gst.State.PLAYING)
+
+    def start(self, w):
+        if self.session_name_changed:
+            self.pipeline.set_state(Gst.State.NULL)
+            self.start_pipeline()
+            self.session_name_changed = False
+        self.pipeline.set_state(Gst.State.PLAYING)
+
+    def stop(self, w):
+        self.pipeline.set_state(Gst.State.NULL)
+
+    def on_message(self, bus, message):
+        t = message.type
+        if t == Gst.MessageType.EOS:
+            self.pipeline.set_state(Gst.State.NULL)
+        elif t == Gst.MessageType.ERROR:
+            err, debug = message.parse_error()
+            print("Error: %s" % err, debug)
+            self.pipeline.set_state(Gst.State.NULL)
+
+
+
+Gst.init(None)
+GTK_Main()
+GObject.threads_init()
+Gtk.main()
+
+print("finish!")
---
+++ rcv_mc_from.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python3
+
+import socket
+import struct
+import sys
+import time
+from threading import Thread
+
+DEBUG = False
+
+class SapReceiver(Thread):
+
+    def __init__(self, m_group='224.2.127.254', server_address=('', 9875)):
+        Thread.__init__(self)
+        self.multicast_group = m_group
+        self.server_address = server_address
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(self.server_address)
+
+        self.group = socket.inet_aton(self.multicast_group)
+        self.mreq = struct.pack('4sL', self.group, socket.INADDR_ANY)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, self.mreq)
+
+        self.sdp_dict = {}
+
+
+    def run(self):
+        sdp_model = SdpModel()
+        change = False
+        while True:
+            data, address = self.sock.recvfrom(1024)
+            ip, port = address
+            if DEBUG : print('received bytes from:'+ str(len(data)) +", "+ ip +"/"+ str(port) + "\n")
+            type = data[0] & 0x04
+            sdp_model.sdp_parser(data[8:].decode())
+            if type==4:
+                change = self.remove_sdp_from(sdp_model)
+            else:
+                change = self.add_new_sdp(sdp_model)
+            if change:
+                change = False
+                if DEBUG : print(self.sdp_dict)
+            time.sleep(0.5)
+
+    def add_new_sdp(self, sdp):
+        if sdp.session_name not in self.sdp_dict.keys():
+            if DEBUG : print('T=Announce')
+            self.sdp_dict[sdp.session_name] = sdp
+            if DEBUG : print(str(sdp))
+            return True
+        return False
+
+    def remove_sdp_from(self, sdp):
+        if sdp.session_name in self.sdp_dict.keys():
+            if DEBUG : print('T=Delete')
+            del self.sdp_dict[sdp.session_name]
+            return True
+        return False
+
+
+class SdpModel:
+
+    def __init__(self):
+        self.ip=""
+        self.port=5004
+        self.session_id=""
+        self.encoding_type=""
+        self.video_prop=""
+        self.payload_type=96
+        self.session_name=""
+
+    def __str__(self):
+        if DEBUG : print("to_string")
+        string = "ip = " + self.ip + "\n" + \
+              "port = " + str(self.port) + "\n" + \
+              "session_id = " + self.session_id + "\n" + \
+              "encoding_type = " + self.encoding_type + "\n" + \
+              "payload_type = " + str(self.payload_type) + "\n" + \
+              "session_name = " + self.session_name + "\n" + \
+              "video_prop = " + self.video_prop + "\n"
+        return string
+
+    def sdp_parser(self, sdp):
+        lines = sdp.splitlines()
+
+        for line in lines:
+            self.get_session_id(line)
+            self.get_session_name(line)
+            self.get_video_port(line)
+            self.get_mcast_ip(line)
+            self.get_encoding_type(line)
+            self.get_video_props(line)
+
+    def get_session_id(self, o_line_from_sdp):
+        elements = o_line_from_sdp.split(' ')
+        if elements[0] == 'o=-':
+            self.session_id = elements[1] + elements[2]
+
+    def get_session_name(self, s_line_from_sdp):
+        elements = s_line_from_sdp.split('=')
+        if elements[0] == 's':
+            self.session_name = elements[1]
+
+    def get_video_port(self, m_line_from_sdp):
+        elements = m_line_from_sdp.split(' ')
+        if elements[0] == 'm=':
+            self.port = int(elements[1])
+            self.payload_type = int(elements[3])
+
+    def get_mcast_ip(self,c_line_from_sdp):
+        elements = c_line_from_sdp.split(' ')
+        if elements[0] == 'c=IN':
+            self.ip = elements[2].split('/')[0]
+
+    def get_encoding_type(self, a_line_from_sdp):
+        elements = a_line_from_sdp.split(' ')
+        if elements[0] == 'a=rtpmap:'+str(self.payload_type):
+            self.encoding_type = elements[1].split('/')[0]
+
+    def get_video_props(self, a_line_from_sdp):
+        elements = a_line_from_sdp.split(' ')
+        result = ""
+        if elements[0] == 'a=fmtp:'+str(self.payload_type):
+            if self.encoding_type == "H264":
+                params = elements[1].split('=',1)[1]
+                params = params.replace("=","\\=")
+                params = params.replace(",","\\,")
+                params += '\\"'+ params+'\\"'
+                result += elements[1].split('=',1)[0] + '=' + params
+            else:
+                params = elements[1].split(';')
+                new_params = ""
+                new_params += ', '.join(x.replace('=','=(string)') for x in params)
+                result += new_params
+        self.video_prop = result
+
+    def get_caps_from_sdp(self):
+        string = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)"+self.encoding_type+ \
+              ", "+self.video_prop+", "+"payload=(int)"+str(self.payload_type)
+        return string
